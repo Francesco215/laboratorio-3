453ad935c5e8b66800c26bd345d55d99483e92f8
Abadabba <abadabba@localhost.localdomain>
2018-10-13 13:18:41 +0200

    asd

--- 2-filtri_passivi_RC/analisi.py
+++ 2-filtri_passivi_RC/analisi.py
@@ -4,41 +4,41 @@ import pylab as pl
 from scipy.optimize import curve_fit
 
 def attenuazione_passa_basso(f,ft,dft):
-	A=1/np.sqrt(1+f**2/ft**2)
-	dA=(1+f**2/ft**2)**(-3/2)*f**2*ft**(-3)*dft
-	return A,dA
+    A=1/np.sqrt(1+f**2/ft**2)
+    dA=(1+f**2/ft**2)**(-3/2)*f**2*ft**(-3)*dft
+    return A,dA
 
 def attenuazione_passa_alto(f,ft,dft):
-	A=1/np.sqrt(1-ft**2/f**2)
-	dA=(1-ft**2/f**2)**(-3/2)*ft*f**(-2)*dft
-	return A,dA
+    A=1/np.sqrt(1-ft**2/f**2)
+    dA=(1-ft**2/f**2)**(-3/2)*ft*f**(-2)*dft
+    return A,dA
 
 def errore_rapporto(x,dx,y,dy):
     return(y**(-2))*np.sqrt(x**2*dy**2+y**2*dx**2)
 
 def lineare(x,q,m):
-	return q+m*x
+    return q+m*x
 
 def frequenza_di_taglio(R,C,dR,dC):
-	ft=1/(R*C*2*np.pi)
-	dft=np.sqrt(dR**2*C**2+dC**2*R**2)/(R*C*2*np.pi)
-	return ft,dft
+    ft=1/(R*C*2*np.pi)
+    dft=np.sqrt(dR**2*C**2+dC**2*R**2)/(R*C*2*np.pi)
+    return ft,dft
 
 """funzione che calcola l'intersezione ed errore tra due rette indipendenti
-	con equazioni y=x*m1+q1 e y=x*m2+q2"""
+    con equazioni y=x*m1+q1 e y=x*m2+q2"""
 
 def int_rette(popt1,popt2,pcov1,pcov2):
-	q1,q2=popt1[0],popt2[0]
-	m1,m2=popt1[1],popt2[1]
-	pcov=np.zeros((4,4))
-	pcov[:2,:2]=pcov1
-	pcov[2:,2:]=pcov2
-	gradientex=([1/(m1-m2),-(q1-q2)/(m1-m2)**2,
-			   -1/(m1-m2), (q1-q2)/(m1-m2)**2])
-	x=(q2-q1)/(m1-m2)
-	y=(q2*m1-q1*m2)/(m1-m2)
-	dx=np.sqrt(np.linalg.multi_dot([gradientex,pcov,gradientex]))
-	return ([x,y]),dx
+    q1,q2=popt1[0],popt2[0]
+    m1,m2=popt1[1],popt2[1]
+    pcov=np.zeros((4,4))
+    pcov[:2,:2]=pcov1
+    pcov[2:,2:]=pcov2
+    gradientex=([1/(m1-m2),-(q1-q2)/(m1-m2)**2,
+               -1/(m1-m2), (q1-q2)/(m1-m2)**2])
+    x=(q2-q1)/(m1-m2)
+    y=(q2*m1-q1*m2)/(m1-m2)
+    dx=np.sqrt(np.linalg.multi_dot([gradientex,pcov,gradientex]))
+    return ([x,y,dx])
 
 #resistenze e capacità con errori
 R1=3.29e3
@@ -69,7 +69,7 @@ popt2,pcov2=curve_fit(lineare,np.log10(freq[:4]),np.log10(A[:4]))
 x=np.linspace(1,4,10)
 pl.plot(10**x,10**(lineare(x,*popt2)))
 #l'intersezione delle due rette
-x,y=int_rette(popt1,popt2,pcov1,pcov2)[0]
+x,y,dx=int_rette(popt1,popt2,pcov1,pcov2)
 #print(int_rette(popt1,popt2,pcov1,pcov2)) 
 pl.plot(10**x,10**y,'.')
 
@@ -79,7 +79,6 @@ pl.ylabel('Vout/Vin')
 pl.xscale('log')
 pl.xlabel('frequenza')
 pl.savefig('figure/a.png')
-pl.close()
 
 
 #punto 2
