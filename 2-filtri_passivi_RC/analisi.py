bb8ff25df432028aa748659b4d7141e36fcc2aaa
Francesco Sacco <francesco215@live.it>
2018-10-13 12:57:44 +0200

    manca il punto 6 per l analisi dati, lorè scrivi il un file chiamato 6.txt i dati che ti sei raccolti in lab

--- 2-filtri_passivi_RC/analisi.py
+++ 2-filtri_passivi_RC/analisi.py
@@ -3,9 +3,14 @@ import menzalib as mz
 import pylab as pl
 from scipy.optimize import curve_fit
 
-def attenuazione(f,ft,dft):
+def attenuazione_passa_basso(f,ft,dft):
 	A=1/np.sqrt(1+f**2/ft**2)
 	dA=(1+f**2/ft**2)**(-3/2)*f**2*ft**(-3)*dft
+	return A,dA
+
+def attenuazione_passa_alto(f,ft,dft):
+	A=1/np.sqrt(1-ft**2/f**2)
+	dA=(1-ft**2/f**2)**(-3/2)*ft*f**(-2)*dft
 	return A,dA
 
 def errore_rapporto(x,dx,y,dy):
@@ -13,6 +18,11 @@ def errore_rapporto(x,dx,y,dy):
 
 def lineare(x,q,m):
 	return q+m*x
+
+def frequenza_di_taglio(R,C,dR,dC):
+	ft=1/(R*C*2*np.pi)
+	dft=np.sqrt(dR**2*C**2+dC**2*R**2)/(R*C*2*np.pi)
+	return ft,dft
 
 """funzione che calcola l'intersezione ed errore tra due rette indipendenti
 	con equazioni y=x*m1+q1 e y=x*m2+q2"""
@@ -23,27 +33,26 @@ def int_rette(popt1,popt2,pcov1,pcov2):
 	pcov=np.zeros((4,4))
 	pcov[:2,:2]=pcov1
 	pcov[2:,2:]=pcov2
-	gradiente=([1/(m1*m2),-(q1*q2)/(m1*m2)**2,
-			   -1/(m1*m2), (q1*q2)/(m1*m2)**2])
-
+	gradientex=([1/(m1-m2),-(q1-q2)/(m1-m2)**2,
+			   -1/(m1-m2), (q1-q2)/(m1-m2)**2])
 	x=(q2-q1)/(m1-m2)
 	y=(q2*m1-q1*m2)/(m1-m2)
-	dx=np.sqrt(np.linalg.multi_dot([gradiente,pcov,gradiente]))
+	dx=np.sqrt(np.linalg.multi_dot([gradientex,pcov,gradientex]))
 	return ([x,y]),dx
 
 #resistenze e capacità con errori
-R=3.29e3
-C=9.88e-9
-dR=mz.dRdig(R)
-dC=mz.dCdig(C)
+R1=3.29e3
+C1=9.88e-9
+dR1=mz.dRdig(R1)
+dC1=mz.dCdig(C1)
 
 #errore frequenza di taglio (punto 1a)
-Ft=1/(R*C*2*np.pi)
-dFt=np.sqrt(dR**2*C**2+dC**2*R**2)/(R*C*2*np.pi)
+Ft1,dFt1=frequenza_di_taglio(R1,C1,dR1,dC1)
 
 #Attenzuazione e errore (punto 1c e 1d)
-A1=([attenuazione(2e3,Ft,dFt),attenuazione(2e4,Ft,dFt)])
+A1=([attenuazione_passa_basso(2e3,Ft1,dFt1),attenuazione_passa_basso(2e4,Ft1,dFt1)])
 
+#qua faccio il plot del diagramma di bode
 Vout,freq=np.genfromtxt('dati/2.txt',unpack=True)
 Vin=12.5
 dVin=mz.dVosc(12.5)
@@ -61,25 +70,32 @@ x=np.linspace(1,4,10)
 pl.plot(10**x,10**(lineare(x,*popt2)))
 #l'intersezione delle due rette
 x,y=int_rette(popt1,popt2,pcov1,pcov2)[0]
-pl.plot(10**x,10**y,'bo')
+#print(int_rette(popt1,popt2,pcov1,pcov2)) 
+pl.plot(10**x,10**y,'.')
+
 
 pl.yscale('log')
 pl.ylabel('Vout/Vin')
 pl.xscale('log')
 pl.xlabel('frequenza')
 pl.savefig('figure/a.png')
-pl.show()
 pl.close()
 
 
-#inizo punto 2
+#punto 2
 Vin,Vout,freq=np.genfromtxt('dati/2a.txt',unpack=True)
 dVin=mz.dVosc(Vin)
 dVout=mz.dVosc(Vout)
-
 Amis=Vout/Vin
 dAmis=errore_rapporto(Vout,dVout,Vin,dVin)
 #print(Amis,dAmis,A1)#stampo il guadagno misurato e il guadagno teorico
 
-#print(int_rette(popt1,popt2,pcov1,pcov2))
-#print(10**int_rette(popt1,popt2,pcov1,pcov2)[0][0])
+R2=3.3e3
+C2=111.7e-9
+dR2=mz.dRdig(R2)
+dC2=mz.dCdig(C2)
+
+Ft2,dFt2=frequenza_di_taglio(R2,C2,dR2,dC2)
+#il punto 5b poi si fa a cazzo
+
+
