bb7b421e4163131e50c5b39e773bff9ad763719d
Lorenzo <lorenzocav97@gmail.com>
2018-11-21 00:42:37 +0100

    asd

--- 6-OpAmp_non_lineare/analisi.py
+++ 6-OpAmp_non_lineare/analisi.py
@@ -6,30 +6,45 @@ from scipy.optimize import curve_fit
 def lineare(x, a, b):
     return a*x+b
 
+def logaritmico(x, a, b):
+    f = a*np.log(b*x)
+#   f[f<0]=0
+    return f
+
+def dlogaritmico(x, a, b):
+    return a/x
+
+
 #2c-1
 Vin, tmin, tmax = np.genfromtxt("dati/1c.txt", unpack=True)
-tmed, dtmed = (tmin+tmax)/2, (tmax-tmin)/2
-pl.errorbar(Vin, tmed, dtmed, fmt='.', label="Misure")
+#tmin, tmax = tmin*10**-6, tmax*10**-6
+
+dVin, dtmax = mz.dVosc(Vin), mz.dtosc(tmax)
+tmax, dtmax = tmax, mz.dtosc(tmax)
+popt, pcov, dpopt, chi2, pvalue = mz.curve_fitdx(logaritmico, Vin, tmax, dVin, dtmax, dlogaritmico)
+print(popt,dpopt, chi2/2)
+
+t = np.linspace(0.2, 10, 2000)
+pl.errorbar(Vin, tmax, dtmax, dVin, fmt='.', label="Misure")
+pl.plot(t, logaritmico(t, *popt), label="Fit")
 pl.xlabel("Vin [V]")
 pl.ylabel("Durata segnale in uscita [us]")
 pl.legend()
-pl.savefig("dati/1c.png")
+#pl.savefig("dati/1c.png")
 pl.close()
 
 #2c-2
 Vdiscr, Vpot = np.genfromtxt("dati/1c-2.txt", unpack=True)
 dVpot, dVdiscr = mz.dVdig(Vpot), mz.dVosc(Vdiscr)
-popt, pcov = mz.curve_fitdx(lineare, Vpot, Vdiscr, dVpot, dVdiscr)
-chi2, pvalue = mz.chi2_pval(lineare, Vpot, Vdiscr, dVdiscr, popt, dVpot)
-dpopt = np.sqrt(np.diag(pcov))
+popt, pcov, dpopt, chi2, pvalue = mz.curve_fitdx(lineare, Vpot, Vdiscr, dVpot, dVdiscr)
 print("a={:.3}+-{:.3} b={:.3}+-{:.3}".format(popt[0],dpopt[0],popt[1],dpopt[1]))
 print("Chi2rid=%0.2f" %(chi2/3))
 
 t = np.linspace(0, 2, 1000)
-pl.plot(t, lineare(t, *popt), label="Fit")
 pl.errorbar(Vpot, Vdiscr, dVdiscr, dVpot, fmt='.', label="Misure")
+pl.plot(t, lineare(t, *popt), label="Fit")
 pl.xlabel("Vpot [V]")
-pl.ylabel("Vdiscr [V]")
+pl.ylabel("Vs [V]")
 pl.legend()
 pl.savefig("dati/1c-2.png")
-pl.close()
+pl.show()
